4031c534ffde1d05b568d0b0be583624ff997042
Sindre Sorhus <sindresorhus@gmail.com>
2015-02-17 10:04:42 +0700

    init

---
+++ test.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+var util = require('util');
+var pathIsAbsolute = require('./');
+
+var path = {
+	posix: {
+		isAbsolute: pathIsAbsolute.posix
+	},
+	win32: {
+		isAbsolute: pathIsAbsolute.win32
+	}
+};
+
+// https://github.com/joyent/node/blob/b3fcc245fb25539909ef1d5eaa01dbf92e168633/test/simple/test-path.js#L344
+assert.equal(path.win32.isAbsolute('//server/file'), true);
+assert.equal(path.win32.isAbsolute('\\\\server\\file'), true);
+assert.equal(path.win32.isAbsolute('C:/Users/'), true);
+assert.equal(path.win32.isAbsolute('C:\\Users\\'), true);
+assert.equal(path.win32.isAbsolute('C:cwd/another'), false);
+assert.equal(path.win32.isAbsolute('C:cwd\\another'), false);
+assert.equal(path.win32.isAbsolute('directory/directory'), false);
+assert.equal(path.win32.isAbsolute('directory\\directory'), false);
+
+assert.equal(path.posix.isAbsolute('/home/foo'), true);
+assert.equal(path.posix.isAbsolute('/home/foo/..'), true);
+assert.equal(path.posix.isAbsolute('bar/'), false);
+assert.equal(path.posix.isAbsolute('./baz'), false);
